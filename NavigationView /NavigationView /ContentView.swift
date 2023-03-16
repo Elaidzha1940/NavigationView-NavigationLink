8072e1ce3225e6cd4eaa2a3bd303878fd1df88bd
Elaidzha1940 <eli080293@gmail.com>
2023-03-16 15:44:38 +0700

    NavigationView / NavigationLink

---
+++ NavigationView /NavigationView /ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  NavigationView 
+//
+//  Created by Elaidzha Shchukin on 16.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+        
+            
+            
+        }
+       // .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NavigationView /NavigationView /NavigationView_App.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationView_App.swift
+//  NavigationView 
+//
+//  Created by Elaidzha Shchukin on 16.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationView_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
