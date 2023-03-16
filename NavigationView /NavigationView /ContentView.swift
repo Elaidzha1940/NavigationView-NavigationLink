c7e2bd0b9f5e0982783943714d7004879518692c
Elaidzha1940 <eli080293@gmail.com>
2023-03-16 15:48:06 +0700

    NavigationView / NavigationLink

--- NavigationView /NavigationView /ContentView.swift
+++ NavigationView /NavigationView /ContentView.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-        
+    
+        NavigationView {
             
+            NavigationLink(
             
         }
-       // .padding()
+        .navigationTitle("Potfolio")
     }
 }
 
